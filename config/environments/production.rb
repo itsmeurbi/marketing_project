7915a0b41bff5b42771a53660695c341aaebe5a6
cinthyaflores <lizeth.clfc@gmail.com>
2019-10-10 09:59:12 -0500

    sendgrid issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,18 +62,11 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Marketing_project_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_charset = "utf-8"
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'marketing-project.herokuapp.com' }
-
-  config.action_mailer.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'marketing-project.herokuapp.com',
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
